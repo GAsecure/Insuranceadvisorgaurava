1538bf0d59072145bb57cf0259f8dc7f545e319d
GAsecure <insuranceadvisorgaurava@gmail.com>
2026-07-14 06:33:30 +0530

    Implement service worker with caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,97 @@
+const CACHE_NAME = "ga-secure-v1";
 
+const urlsToCache = [
+
+  "./",
+
+  "./index.html",
+
+  "./style.css",
+
+  "./script.js",
+
+  "./manifest.json",
+
+  "./about.html",
+
+  "./health.html",
+
+  "./motor.html",
+
+  "./life.html",
+
+  "./home.html",
+
+  "./claim.html",
+
+  "./quote.html",
+
+  "./download.html",
+
+  "./contact.vcf",
+
+  "./assets/gaurav.png",
+
+  "./assets/qr.png",
+
+  "./assets/card.jpg",
+
+  "./assets/icon-192.png",
+
+  "./assets/icon-512.png"
+
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+
+        .then(cache => cache.addAll(urlsToCache))
+
+    );
+
+});
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys => {
+
+            return Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+});
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
